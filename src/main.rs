05511378379aafbd7db573b4076f7dbc17d2c0a1
Nate <natenethercott@gmail.com>
2026-04-29 14:58:49 +0200

    feat: driver/actor pattern

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(unsafe_cell_access)]
+
+use std::cell::UnsafeCell;
 use std::io::{self, Read};
 use std::{
     collections::HashMap,
@@ -5,23 +8,20 @@ use std::{
     fs::File,
     path::Path,
     process::{self, Stdio},
-    sync::{
-        atomic::{AtomicI64, Ordering},
-        LazyLock,
-    },
+    sync::atomic::{AtomicI64, Ordering},
 };
 
 use serde_json::{json, Value};
+use tokio::io::AsyncWriteExt;
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::oneshot;
 use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    io::{AsyncBufReadExt, AsyncReadExt, BufReader},
     process::{Child, ChildStdin, ChildStdout, Command},
-    sync::Mutex,
 };
 use tower_lsp::jsonrpc::{self};
 
 static REQUEST_ID: AtomicI64 = AtomicI64::new(0);
-static RESPONSE_REGISTRY: LazyLock<Mutex<HashMap<jsonrpc::Id, jsonrpc::Response>>> =
-    LazyLock::new(|| Mutex::new(HashMap::new()));
 
 fn next_id() -> i64 {
     REQUEST_ID.fetch_add(1, Ordering::Relaxed)
@@ -41,6 +41,9 @@ enum Error {
     #[error(transparent)]
     Serde(#[from] serde_json::Error),
 
+    #[error("failed to send/recv event")]
+    Tokio,
+
     #[allow(dead_code)]
     #[error("something happened")]
     Other,
@@ -49,7 +52,27 @@ enum Error {
 type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
-struct LspServer {
+enum Action {
+    Request {
+        req: jsonrpc::Request,
+        tx: oneshot::Sender<jsonrpc::Response>,
+    },
+    Notify {
+        req: jsonrpc::Request,
+    },
+}
+
+impl Action {
+    pub fn contents(&self) -> &jsonrpc::Request {
+        match self {
+            Action::Request { req, .. } => req,
+            Action::Notify { req } => req,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Lsp {
     name: String,
     args: Option<Vec<String>>,
 }
@@ -60,19 +83,19 @@ macro_rules! lsp {
             let mut v = Vec::new();
             $(v.push(format!("{}", $arg)))*;
             let args = if v.is_empty() {None } else { Some(v) };
-            LspServer{ name: format!("{}", $name), args }
+            Lsp{ name: format!("{}", $name), args }
         }
     };
 }
 
-struct LspClient {
-    pub proc: Child,
-    stdout: Mutex<BufReader<ChildStdout>>,
-    stdin: Mutex<ChildStdin>,
+struct LspServer {
+    proc: Child,
+    stdout: BufReader<ChildStdout>,
+    stdin: ChildStdin,
 }
 
-impl LspClient {
-    pub fn new(lsp: LspServer) -> Result<Self> {
+impl LspServer {
+    pub fn new(lsp: Lsp) -> Result<Self> {
         dbg!(&lsp);
 
         let mut lsp_handle = Command::new(&lsp.name)
@@ -86,13 +109,109 @@ impl LspClient {
 
         Ok(Self {
             proc: lsp_handle,
-            stdin: Mutex::new(stdin),
-            stdout: Mutex::new(BufReader::new(stdout)),
+            stdin: stdin,
+            stdout: BufReader::new(stdout),
         })
     }
 
-    pub async fn init(&mut self, path: &str) -> Result<()> {
-        self.request_or_notify(
+    async fn read_one(&mut self) -> Result<Value> {
+        let mut buf = String::new();
+        let mut size: usize = 0;
+
+        // process headers
+        loop {
+            buf.clear();
+            self.stdout.read_line(&mut buf).await?;
+            if buf.trim().is_empty() {
+                break;
+            }
+            if buf.contains("Content-Length") {
+                let (_, l) = buf.split_once("Content-Length: ").unwrap();
+                size = l.trim().parse().unwrap();
+            }
+        }
+
+        let mut response = vec![0; size];
+        self.stdout.read_exact(&mut response).await?;
+
+        let resp = serde_json::from_slice::<Value>(&response)?;
+        Ok(resp)
+    }
+
+    async fn write_one(&mut self, msg: &str) -> io::Result<usize> {
+        let msg = format!("Content-Length: {}\r\n\r\n{}", msg.len(), msg);
+        self.stdin.write(msg.as_bytes()).await
+    }
+}
+
+impl Drop for LspServer {
+    fn drop(&mut self) {
+        let _ = self.proc.start_kill();
+    }
+}
+
+struct Driver {
+    server: LspServer,
+    incoming: Receiver<Action>,
+    sendback_channels: HashMap<jsonrpc::Id, oneshot::Sender<jsonrpc::Response>>,
+}
+
+impl Driver {
+    pub fn new(server: LspServer, rx: Receiver<Action>) -> Self {
+        Self {
+            server,
+            incoming: rx,
+            sendback_channels: HashMap::new(),
+        }
+    }
+
+    async fn enqueue(&mut self) -> Result<()> {
+        while let Some(act) = self.incoming.recv().await {
+            let msg = serde_json::to_string(act.contents())?;
+
+            let _ = match act {
+                Action::Request { req, tx } => {
+                    let id = req.id().unwrap();
+                    let _ = self.sendback_channels.insert(id.clone(), tx);
+
+                    self.server.write_one(&msg).await.map_err(|e| {
+                        let _ = self.sendback_channels.remove(id);
+                        e
+                    })?
+                }
+                Action::Notify { .. } => self.server.write_one(&msg).await?,
+            };
+        }
+        Ok(())
+    }
+
+    async fn dequeue(&mut self) -> Result<()> {
+        loop {
+            let res = self.server.read_one().await?;
+            let response = serde_json::from_value::<jsonrpc::Response>(res);
+
+            // read past notifications
+            if response.is_err() {
+                continue;
+            }
+
+            let parsed = response?;
+
+            let tx = self
+                .sendback_channels
+                .remove(parsed.id())
+                .ok_or(Error::Other)?;
+
+            tx.send(parsed).map_err(|_| Error::Tokio)?;
+        }
+    }
+}
+
+struct LspClient(Sender<Action>);
+
+impl LspClient {
+    pub async fn init(&self, path: &str) -> Result<()> {
+        self.request(
             jsonrpc::Request::build("initialize")
                 .id(next_id())
                 .params(json!({
@@ -104,146 +223,109 @@ impl LspClient {
         )
         .await?;
 
-        // ok; initialized
-        self.request_or_notify(jsonrpc::Request::build("initialized").finish())
+        self.notify(jsonrpc::Request::build("initialized").finish())
             .await?;
 
         Ok(())
     }
 
-    async fn send(&self, msg: &str) -> io::Result<usize> {
-        let msg = format!("Content-Length: {}\r\n\r\n{}", msg.len(), msg);
-        self.stdin.lock().await.write(msg.as_bytes()).await
+    pub async fn request(&self, req: jsonrpc::Request) -> Result<jsonrpc::Response> {
+        let (tx, rx) = tokio::sync::oneshot::channel();
+        let act = Action::Request { req, tx };
+
+        self.0.send(act).await.map_err(|_| Error::Tokio)?;
+        rx.await.map_err(|_| Error::Tokio)
     }
 
-    // the external reader steps lsp subproc stdout read state
-    async fn recv(&self) -> Result<Value> {
-        let mut stdout_guard = self.stdout.lock().await;
-        let mut buf = String::new();
-
-        let mut size: usize = 0;
-
-        // process headers
-        loop {
-            buf.clear();
-            stdout_guard.read_line(&mut buf).await?;
-            if buf.trim().is_empty() {
-                break;
-            }
-            if buf.contains("Content-Length") {
-                let (_, l) = buf.split_once("Content-Length: ").unwrap();
-                size = l.trim().parse().unwrap();
-            }
-        }
-
-        let mut response = vec![0; size];
-        stdout_guard.read_exact(&mut response).await?;
-
-        drop(stdout_guard);
-
-        let resp = serde_json::from_slice::<Value>(&response)?;
-        Ok(resp)
-    }
-
-    pub async fn request_or_notify(
-        &self,
-        req: jsonrpc::Request,
-    ) -> Result<Option<jsonrpc::Response>> {
-        self.send(&req.to_string()).await?;
-
-        // FIXME: here we're aiming for an atomic "read stdout and update global registry"
-        // but its bad since a) worst case everything is inserted in RAM into `RESPONSE_REGISTRY` 
-        // and b) holding the registry mutex guard is blocking.
-        // I don't, however, want to decouple the stdout read from the send op since i feel like
-        // there's a clever way to do this
-        match req.id() {
-            Some(id) => loop {
-                let mut registry_guard = RESPONSE_REGISTRY.lock().await;
-
-                if let Some(value) = registry_guard.remove(id) {
-                    return Ok(Some(value));
-                }
-
-                let res = self.recv().await?;
-                let response = serde_json::from_value::<jsonrpc::Response>(res);
-                if response.is_err() {
-                    continue;
-                }
-
-                let parsed = response?;
-
-                if parsed.id() != id {
-                    registry_guard.insert(parsed.id().clone(), parsed);
-                    continue;
-                }
-                return Ok(Some(parsed));
-            },
-            // request was a notification
-            None => Ok(None),
-        }
+    pub async fn notify(&self, req: jsonrpc::Request) -> Result<()> {
+        let act = Action::Notify { req };
+        self.0.send(act).await.map_err(|_| Error::Tokio)?;
+        Ok(())
     }
 }
 
-// impl Drop for LspClient {
-//     fn drop(&mut self) {
-//         // FIXME: do i need to box leak this somehow?
-//         let drop_fut = async move || self.proc.kill().await.expect("failed to terminate lsp");
-//         tokio::spawn(drop_fut());
-//     }
-// }
+async fn serve(instance: &mut Driver) -> Result<()> {
+    // SAFETY: LspServer::read_one steps the child processes stdout read state, but we only
+    // do this single threaded. c.f. for LspServer::write_one.
+    // We should actually split ownership of the read/write fds into different structs, but flemme
+    let unsafe_instance = UnsafeCell::new(instance);
+
+    let write_fut = unsafe { unsafe_instance.as_mut_unchecked().enqueue() };
+    let read_fut = unsafe { unsafe_instance.as_mut_unchecked().dequeue() };
+
+    tokio::select! {
+       res = write_fut => res,
+       res = read_fut => res,
+    }
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let mut client = LspClient::new(lsp!("ty", "server"))?;
+    let (tx, rx) = tokio::sync::mpsc::channel(8);
 
-    let py_dir = current_dir().map(|dir| dir.join("python")).unwrap();
-    client.init(py_dir.to_str().unwrap()).await?;
-
-    let open_doc = |doc_uri: &str, contents: String| {
-        jsonrpc::Request::build("textDocument/didOpen")
-            .params(json!({
-                "textDocument": {
-                    "uri": doc_uri,
-                    "languageId": "python",
-                    "version": 0,
-                    "text": contents,
-                }
-            }))
-            .finish()
+    let actor = async move || {
+        let server = LspServer::new(lsp!("ty", "server"))?;
+        let mut driver = Driver::new(server, rx);
+        serve(&mut driver).await?;
+        Ok::<(), Error>(())
     };
 
-    let file = py_dir.join("nate.py");
-    let file_uri = format!("file://{}", file.to_str().unwrap());
-    let contents = get_contents("python/nate.py")?;
+    let driver = async || {
+        let client = LspClient(tx);
 
-    client
-        .request_or_notify(open_doc(&file_uri, contents))
-        .await?;
+        let py_dir = current_dir().map(|dir| dir.join("python")).unwrap();
+        dbg!(&py_dir);
+        client.init(py_dir.to_str().unwrap()).await?;
 
-    let ref_factory = |doc_uri: &str, line: usize, char: usize| {
-        jsonrpc::Request::build("textDocument/references")
-            .id(next_id())
-            .params(json!({
-                "textDocument": {
-                    "uri": doc_uri,
-                },
-                "position": {
-                    "line": line,
-                    "character": char,
-                },
-                "context": {
-                    "includeDeclaration": true,
-                }
-            }))
-            .finish()
+        let open_doc = |doc_uri: &str, contents: String| {
+            jsonrpc::Request::build("textDocument/didOpen")
+                .params(json!({
+                    "textDocument": {
+                        "uri": doc_uri,
+                        "languageId": "python",
+                        "version": 0,
+                        "text": contents,
+                    }
+                }))
+                .finish()
+        };
+
+        let file = py_dir.join("nate.py");
+        let file_uri = format!("file://{}", file.to_str().unwrap());
+        let contents = get_contents("python/nate.py")?;
+
+        client.notify(open_doc(&file_uri, contents)).await?;
+
+        let ref_factory = |doc_uri: &str, line: usize, char: usize| {
+            jsonrpc::Request::build("textDocument/references")
+                .id(next_id())
+                .params(json!({
+                    "textDocument": {
+                        "uri": doc_uri,
+                    },
+                    "position": {
+                        "line": line,
+                        "character": char,
+                    },
+                    "context": {
+                        "includeDeclaration": true,
+                    }
+                }))
+                .finish()
+        };
+
+        let res = tokio::join!(
+            client.request(ref_factory(&file_uri, 0, 5)),
+            client.request(ref_factory(&file_uri, 0, 5)),
+        );
+        let _ = dbg!(res);
+        Ok::<(), Error>(())
     };
 
-    let _res = tokio::join!(
-        client.request_or_notify(ref_factory(&file_uri, 0, 5)),
-        client.request_or_notify(ref_factory(&file_uri, 0, 5)),
-    );
-
-    client.proc.kill().await.expect("failed to kill subproc");
+    tokio::select! {
+        _ = actor() => (),
+        _ = driver() => (),
+    }
 
     Ok(())
 }
